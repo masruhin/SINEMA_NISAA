c52af541bc9ab8731e2f672dc06d8280c3643d69
masruhin <masruhin15201029@gmail.com>
2022-11-20 22:28:34 +0700

    begadang db

--- db/sinema.sql
+++ db/sinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2022 at 04:28 PM
+-- Generation Time: Nov 20, 2022 at 04:27 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 5.6.39
 
@@ -25,6 +25,115 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `negara`
+--
+
+CREATE TABLE `negara` (
+  `id` int(11) NOT NULL,
+  `negara_nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `negara`
+--
+
+INSERT INTO `negara` (`id`, `negara_nama`) VALUES
+(9, 'Laosaaa'),
+(11, 'Jerman'),
+(12, 'Amerika as'),
+(13, 'Taiwan'),
+(15, 'solo'),
+(16, 'Rusia'),
+(17, 'Rusia'),
+(20, 'kicik'),
+(21, 'edan'),
+(22, 'jancuk'),
+(23, 'senegal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `negara_kategori`
+--
+
+CREATE TABLE `negara_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori_nama` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `negara_kategori`
+--
+
+INSERT INTO `negara_kategori` (`id_kategori`, `kategori_nama`) VALUES
+(1, 'NATIONAL'),
+(2, 'INTERNATIONAL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
+--
+
+CREATE TABLE `unit` (
+  `id_unit` int(11) NOT NULL,
+  `unit_kode` varchar(20) NOT NULL,
+  `unit_nama` varchar(255) NOT NULL,
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit`
+--
+
+INSERT INTO `unit` (`id_unit`, `unit_kode`, `unit_nama`, `date_created`, `date_updated`) VALUES
+(1, '', 'Biro Administrasi Umum', '2022-11-19 17:00:00', '2022-11-20 07:38:35'),
+(2, '', 'Fakultas Ekonomi dan Bisnis', '2022-11-19 17:00:00', NULL),
+(3, '', 'Fakultas Teknik Informatika', '2022-11-20 07:22:46', '2022-11-20 07:38:23'),
+(5, '', 'Pascasarjana', '2022-11-20 14:17:45', NULL),
+(6, '', 'Biro Keuangan', '2022-11-20 14:17:45', NULL),
+(7, '', 'Lembaga Pengembangan Kemahasiswaan dan Alumni', '2022-11-20 14:17:45', NULL),
+(8, '', 'Biro Sumber Daya Insani', '2022-11-20 14:17:45', NULL),
+(9, '', 'Direktorat Pemakmuran Masjid', '2022-11-20 14:17:45', NULL),
+(10, '', 'Satuan Pengendalian Internal', '2022-11-20 14:17:45', NULL),
+(11, '', 'UPT Perpustakaan', '2022-11-20 14:17:45', NULL),
+(12, '', 'UPT Pemasaran dan Kehumasan', '2022-11-20 14:17:45', NULL),
+(13, '', 'Biro Hukum', '2022-11-20 14:17:45', NULL),
+(14, '', 'Biro Administrasi Umum', '2022-11-20 14:17:45', NULL),
+(15, '', 'Biro Administrasi Akademik', '2022-11-20 14:17:45', NULL),
+(16, '', 'Lembaga Penelitian dan Pengembangan Masyarakat', '2022-11-20 14:17:45', NULL),
+(17, 'aa', 'ab', '2022-11-20 15:16:46', '2022-11-20 15:20:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `universitas`
+--
+
+CREATE TABLE `universitas` (
+  `id_univ` int(11) NOT NULL,
+  `univ_nama` varchar(255) NOT NULL,
+  `univ_alamat` text NOT NULL,
+  `univ_telp` varchar(20) NOT NULL,
+  `univ_wa` varchar(20) NOT NULL,
+  `univ_email` varchar(50) NOT NULL,
+  `univ_fax` varchar(50) NOT NULL,
+  `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `universitas`
+--
+
+INSERT INTO `universitas` (`id_univ`, `univ_nama`, `univ_alamat`, `univ_telp`, `univ_wa`, `univ_email`, `univ_fax`, `date_created`, `date_updated`) VALUES
+(1, 'universitas TRISAKTI', 'Jalan Kyai Tapa No. 1 Grogol\r\nJakarta Barat, Indonesia                                              ', '(62-21) 566 3232', '(+62) 882 1948 5674', 'humas@trisakti.ac.id', '(62-21) 564 4270', '2022-11-20 12:21:20', '2022-11-20 14:39:54'),
+(2, 'udinus', 'semarang', '08232323', '0090232322', 'udinus@.gmail.com', '232323', '2022-11-20 13:23:50', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -42,12 +151,36 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `nama_user`, `level`, `blokir`) VALUES
-(1, 'member', '17c4520f6cfd1ab53d8745e84681eb49', 'member', 'user', 'N'),
-(2, 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', 'superadmin', 'admin', 'N');
+(1, 'member', '17c4520f6cfd1ab53d8745e84681eb49', 'member', 'user', 'Y'),
+(2, 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', 'superadmin', 'admin', 'Y');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `negara`
+--
+ALTER TABLE `negara`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `negara_kategori`
+--
+ALTER TABLE `negara_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `unit`
+--
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`id_unit`);
+
+--
+-- Indexes for table `universitas`
+--
+ALTER TABLE `universitas`
+  ADD PRIMARY KEY (`id_univ`);
 
 --
 -- Indexes for table `users`
@@ -58,6 +191,30 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `negara`
+--
+ALTER TABLE `negara`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `negara_kategori`
+--
+ALTER TABLE `negara_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `unit`
+--
+ALTER TABLE `unit`
+  MODIFY `id_unit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `universitas`
+--
+ALTER TABLE `universitas`
+  MODIFY `id_univ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
