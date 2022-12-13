8854da5b0eed16dd06e39cfcd2e950bd8737291f
masruhin <masruhin15201029@gmail.com>
2022-12-13 14:56:55 +0700

    ok

--- admin/kerjasama.php
+++ admin/kerjasama.php
@@ -100,10 +100,18 @@ if(empty($_SESSION['username'])){
                   <td><?php echo $hasil ['no_ref_kerjasama'];?></td>
                   <td><?php echo tanggal_indonesia("2022-02-02");?></td>
                   <td style="text-align:center ;">
-                    <a href="#" type="button" class="open_modal btn btn-outline-info round btn-sm" data-toggle="modal"
-                      data-target="#view<?php echo $hasil['id_kerjasama']; ?>">Lihat</a> |
-                    <a href="#" type="button" class="open_modal btn btn-outline-danger round btn-sm" data-toggle="modal"
-                      data-target="#deleteEmployeeModal<?php echo $hasil['id_kerjasama']; ?>">Hapus</a>
+                    <a href="#" type="button"
+                      class="open_modal btn btn-icon btn-icon rounded-circle btn-primary waves-effect waves-float waves-light btn-sm"
+                      data-toggle="modal" data-target="#view<?php echo $hasil['id_kerjasama']; ?>"><i
+                        data-feather='eye'></i></a>
+                    <a href="#" type="button"
+                      class="open_modal btn btn-icon btn-icon rounded-circle btn-success waves-effect waves-float waves-light btn-sm"
+                      data-toggle="modal" data-target="#edit<?php echo $hasil['id_kerjasama']; ?>"><i
+                        data-feather='edit-3'></i></a>
+                    <a href="#" type="button"
+                      class="open_modal btn btn-icon btn-icon rounded-circle btn-danger waves-effect waves-float waves-light btn-sm"
+                      data-toggle="modal" data-target="#deleteEmployeeModal<?php echo $hasil['id_kerjasama']; ?>"><i
+                        data-feather='trash-2'></i></a>
                   </td>
                 </tr>
 
@@ -306,6 +314,206 @@ if(empty($_SESSION['username'])){
                   </div>
                 </div>
                 <!-- END MODAL VIEw -->
+
+                <!-- MODAL EDIT -->
+                <div class="modal fade text-left" id="edit<?php echo $hasil['id_kerjasama']; ?>" tabindex="-1"
+                  role="dialog" aria-labelledby="myModalLabel16" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title" id="myModalLabel16">View Data</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <form class="form form-horizontal" action="jenis_dok_act.php" method="POST">
+                          <input type="hidden" name="id_kerjasama" value="<?= $row['id_kerjasama']?>">
+                          <?php
+                              $id = $hasil['id_kerjasama']; 
+                              $query_edit = mysqli_query($kon, "SELECT
+                              a.id_kerjasama,
+                              b.id_jenis_dok,
+                              b.jenis_dok,
+                              d.id_fak,
+                              d.fak_kode,
+                              d.fak_nama,
+                              c.id_unit,
+                              c.unit_nama,
+                              a.judul_kerjasama,
+                              a.deskripsi_kerjasama,
+                              a.status_kerjasama,
+                              a.tanggal_awal,
+                              a.tanggal_akhir,
+                              a.no_ref_kerjasama,
+                              a.file,
+                              a.date_created,
+                              a.date_updated 
+                            FROM
+                              kerjasama a
+                              LEFT JOIN jenis_dok b ON b.id_jenis_dok = a.id_jenis_dok
+                              LEFT JOIN fakultas d ON d.id_fak = a.id_fak
+                              LEFT JOIN unit c ON c.id_unit= a.id_unit WHERE id_kerjasama='$id'");
+                              while ($row = mysqli_fetch_array($query_edit)) {  
+                              ?>
+                          <input type="hidden" name="id_kerjasama" value="<?= $row['id_kerjasama']?>">
+                          <section id="input-sizing">
+                            <div class="row match-height">
+                              <div class="col-lg-4 col-md-4">
+                                <div class="card">
+                                  <div class="card-header">
+                                    <div class="accordion list-group-item list-group-item-action active" type="button"
+                                      data-bs-toggle="collapse" data-bs-target="#accordionTwo" aria-expanded="true"
+                                      aria-controls="accordionTwo"><span>Masa Berlaku</span>
+                                    </div>
+                                    <div class="card-body list-group-item" id="accordionTwo">
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="first-name-icon">Status</P>
+                                            <div class="input-group mb-1">
+                                              <div class="input-group-prepend">
+                                                <span class="input-group-text"><i data-feather="pocket"></i></span>
+                                              </div>
+                                              <input type="text" class="form-control"
+                                                value="<?php echo $hasil['status_kerjasama']?>" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="tanggal_awal">Tanggal
+                                              Awal</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather="calendar"></i></span>
+                                              <input type="date" id="tanggal_awal" class="form-control"
+                                                value="<?php echo $hasil ['tanggal_awal'];?>" name="tanggal_awal" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="tanggal_akhir">Tanggal
+                                              Akhir</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather="calendar"></i></span>
+                                              <input type="date" id="tanggal_awal" class="form-control"
+                                                value="<?php echo $hasil ['tanggal_akhir'];?>" name="tanggal_awal" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="fak_nama">Fakultas Penggiat
+                                              Kerjasama</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather="layers"></i></span>
+                                              <input type="text" id="fak_nama" class="form-control"
+                                                value="<?php echo $hasil['fak_nama'];?>" name="fak_nama" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="col-lg-8 col-md-8">
+                                <div class="card">
+                                  <div class="card-header">
+                                    <div class="card-body list-group-item" id="accordionTwo">
+                                      <div class="row">
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="first-name-icon">Jenis Dokumen
+                                              Kerjasama</P>
+                                            <div class="input-group mb-1">
+                                              <div class="input-group-prepend">
+                                                <span class="input-group-text"><i data-feather='link'></i></span>
+                                              </div>
+                                              <input type="text" class="form-control"
+                                                value="<?php echo $hasil['jenis_dok']?>" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="first-name-icon">Judul Kerjasama</P>
+                                            <div class="input-group mb-1">
+                                              <div class="input-group-prepend">
+                                                <span class="input-group-text"><i data-feather='at-sign'></i></span>
+                                              </div>
+                                              <input type="text" class="form-control"
+                                                value="<?php echo $hasil['judul_kerjasama']?>" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="deskripsi_kerjasama">Deskripsi
+                                              Kerjasama</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather="calendar"></i></span>
+                                              <input type="text" id="deskripsi_kerjasama" class="form-control"
+                                                value="<?php echo $hasil ['deskripsi_kerjasama'];?>"
+                                                name="deskripsi_kerjasama" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="tanggal_akhir">No Ref Kerjasama</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather='map-pin'></i></span>
+                                              <input type="text" id="no_ref_kerjasama" class="form-control"
+                                                value="<?php echo $hasil ['no_ref_kerjasama'];?>"
+                                                name="no_ref_kerjasama" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="tanggal_akhir">Tanggal Input</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather='pen-tool'></i></span>
+                                              <input type="text" id="date_created" class="form-control"
+                                                value="<?php echo $hasil ['date_created'];?>" name="date_created" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-12">
+                                          <div class="mb-1">
+                                            <P class="form-label" for="fak_nama">File Dokumen
+                                              Kerjasama</P>
+                                            <div class="input-group ">
+                                              <span class="input-group-text"><i data-feather="file"></i></span>
+                                              <input type="text" id="file" class="form-control"
+                                                value="<?php echo $hasil['file'];?>" name="file" />
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </section>
+                          <div class="col-sm-12 offset-sm-12 modal-footer">
+                            <button type="submit" class="btn btn-info mr-1 btn-sm" name="ubah">Simpan</button>
+                            <button type="reset" class="btn btn-outline-danger btn-sm">Reset</button>
+                          </div>
+                          <?php 
+                              }
+                              //mysql_close($host);
+                              ?>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- END MODAL EDIT -->
+
 
                 <!-- MODAL HAPUS -->
                 <div id="deleteEmployeeModal<?php echo $hasil['id_jenis_dok']; ?>" class="modal fade">
