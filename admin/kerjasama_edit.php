6bb9103ec8d0f76823af2b5f9a0a0a3ce8d340ac
masruhin <masruhin15201029@gmail.com>
2023-01-13 22:32:17 +0700

    edit kerjasama uda oK

--- admin/kerjasama_edit.php
+++ admin/kerjasama_edit.php
@@ -20,12 +20,10 @@ if (empty($_SESSION['username'])) {
             <form action="kerjasama_proses_edit.php" method="post" enctype="multipart/form-data">
               <?php include 'config.php';
               $id			= $_GET['id_kerjasama'];
-              $sql  		= "SELECT a.id_kerjasama, b.id_jenis_dok, b.jenis_dok, d.id_fak, d.fak_kode, d.fak_nama,
-              c.id_unit, c.unit_nama, a.judul_kerjasama,  a.deskripsi_kerjasama, a.status_kerjasama,a.tanggal_awal,
-              a.tanggal_akhir, a.no_ref_kerjasama,a.file,a.date_created,a.date_updated FROM kerjasama a
+              $sql  		= "SELECT a.id_kerjasama, b.id_jenis_dok, b.jenis_dok, b.jenis_ket, d.id_fak, d.fak_kode, d.fak_nama, c.id_unit, c.unit_nama, a.judul_kerjasama,  a.deskripsi_kerjasama, a.status_kerjasama,a.tanggal_awal, a.tanggal_akhir, a.no_ref_kerjasama,a.file,a.date_created,a.date_updated FROM kerjasama a
               LEFT JOIN jenis_dok b ON b.id_jenis_dok = a.id_jenis_dok
               LEFT JOIN fakultas d ON d.id_fak = a.id_fak
-              LEFT JOIN unit c ON c.id_unit= a.id_unit WHERE id_kerjasama = 1 ";                        
+              LEFT JOIN unit c ON c.id_unit= a.id_unit WHERE id_kerjasama ='$id' ";                        
               $query  	= mysqli_query($kon, $sql);
               $data 		= mysqli_fetch_array($query);
               // echo '<pre>';
@@ -42,9 +40,10 @@ if (empty($_SESSION['username'])) {
               $noref          = $data['no_ref_kerjasama'];
               $date_c         = $data['date_created'];
               $date_u         = $data['date_updated'];
+              $file         = $data['file'];
               ?>
 
-              <input type=hidden name="id_suratmasuk" value="<?php echo $id;?>">
+              <input type=hidden name="id_kerjasama" value="<?php echo $id;?>">
               <div class="row">
                 <div class="col-md-12">
                   <div class="card">
@@ -54,18 +53,72 @@ if (empty($_SESSION['username'])) {
 
                         <div class="col-md-6 col-12 mb-1">
                           <div class="form-group row">
-                            <label for="colFormLabel" class="col-sm-3 col-form-label">Fakultas Penggiat</label>
+                            <label for="colFormLabel" class="col-sm-4 col-form-label"><span
+                                class="badge badge-pill badge-light-success mr-1 lg"
+                                style="background-color: rgb(255 205 21 / 83%); height:25px; color: #020202 !important;">Jenis
+                                Dokumen Kerjasama</span></label>
+                            <div class="col-sm-8">
+                              <select name="id_jenis_dok" class="select2 form-control" tabindex="-1">
+                                <option value="<?php echo $data['id_jenis_dok'];?>">
+                                  <?php echo $data['jenis_dok'] ." | ". $data['jenis_ket']; ?>
+                                </option>
+                                <option disabled>-- Pilh untuk mengganti --</option>
+                                <?php include 'config.php';
+                                $sql2  		= "SELECT * FROM jenis_dok";                        
+                                $query2  	= mysqli_query($kon, $sql2);
+                                while ($data2 = mysqli_fetch_array($query2)){?>
+                                <option value="<?php echo $data2['id_jenis_dok'] ?>">
+                                  <?php echo $data2['jenis_dok'] ." | ". $data2['jenis_ket']; ?>
+                                </option>
+                                <!-- echo '<option value="'.$data2['id_jenis_dok'].'">'.$data2['jenis_ket'].'
+                                  |'.$data2['jenis_dok'].'</option>'; -->
+                                <?php }
+                                ?>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="form-group row">
+                            <label for="colFormLabel" class="col-sm-3 col-form-label"><span
+                                class="badge badge-pill badge-light-success mr-1 lg"
+                                style="background-color: rgb(255 205 21 / 83%); height:25px; color: #020202 !important;">Fakultas
+                                Penggiat</span></label>
                             <div class="col-sm-9">
                               <select name="id_fak" class="select2 form-control" tabindex="-1">
-                                <option value="<?php echo $data['fak_nama'];?>">
-                                  <?php echo $data['fak_nama'];?>
+                                <option value="<?php echo $data['id_fak'];?>">
+                                  <?php echo $data['fak_kode'] ." | ". $data['fak_nama']; ?>
                                 </option>
-                                <option>-- Pilh untuk mengganti --</option>
+                                <option disabled>-- Pilh untuk mengganti --</option>
                                 <?php include 'config.php';
-                                $sql2  		= "SELECT fak_nama FROM fakultas";                        
+                                $sql2  		= "SELECT * FROM fakultas";                        
+                                $query2  	= mysqli_query($kon, $sql2);
+                                while ($data2 = mysqli_fetch_array($query2)){?>
+                                <option value="<?php echo $data2['id_fak'] ?>">
+                                  <?php echo $data2['fak_kode'] ." | ". $data2['fak_nama']; ?>
+                                </option>
+                                <!-- echo '<option value="'.$data2['id_fak'].'">'.$data2['fak_nama'].'</option>'; -->
+                                <?php }
+                                ?>
+                              </select>
+                            </div>
+                          </div>
+
+                          <div class="form-group row">
+                            <label for="colFormLabel" class="col-sm-3 col-form-label">
+                              <span class="badge badge-pill badge-light-success mr-1 lg"
+                                style="background-color: rgb(255 205 21 / 83%); height:25px; color: #020202 !important;">Unit
+                                Penggiat</span>
+                            </label>
+                            <div class="col-sm-9">
+                              <select name="id_unit" class="select2 form-control" tabindex="-1">
+                                <option value="<?php echo $data['id_unit'];?>">
+                                  <?php echo $data['unit_nama'];?>
+                                </option>
+                                <option disabled>-- Pilh untuk mengganti --</option>
+                                <?php include 'config.php';
+                                $sql2  		= "SELECT * FROM unit";                        
                                 $query2  	= mysqli_query($kon, $sql2);
                                 while ($data2 = mysqli_fetch_array($query2)){
-                                      echo '<option value="'.$data2['fak_nama'].'">'.$data2['fak_nama'].'</option>';
+                                      echo '<option value="'.$data2['id_unit'].'">'.$data2['unit_nama'].'</option>';
                                       } 
                                 ?>
                               </select>
@@ -73,20 +126,28 @@ if (empty($_SESSION['username'])) {
                           </div>
 
                           <div class="form-group row">
-                            <label for="colFormLabel" class="col-sm-3 col-form-label">Unit Penggiat</label>
-                            <div class="col-sm-9">
-                              <select name="id_unit" class="select2 form-control" tabindex="-1">
-                                <option value="<?php echo $data['unit_nama'];?>">
-                                  <?php echo $data['unit_nama'];?>
+                            <label for="colFormLabel" class="col-sm-4 col-form-label"> <span
+                                class="badge badge-pill badge-light-success mr-1 lg"
+                                style="background-color: rgb(255 205 21 / 83%); height:25px; color: #020202 !important;">No
+                                Referensi Kerjasama</span></label>
+                            <div class="col-sm-8">
+                              <input type="text" class="form-control" id="colFormLabel" name="no_ref_kerjasama"
+                                placeholder="Normal Input" / value="<?= $noref ?>">
+                            </div>
+                          </div>
+
+                          <div class="form-group row">
+                            <label for="colFormLabel" class="col-sm-4 col-form-label"> <span
+                                class="badge badge-pill badge-light-success mr-1 lg"
+                                style="background-color: rgb(255 205 21 / 83%); height:25px; color: #020202 !important;">Status</span></label>
+                            <div class="col-sm-8">
+                              <select name="status" class="select2 form-control" tabindex="-1">
+                                <option value="<?php echo $status;?>">
+                                  <?php echo $status;?>
                                 </option>
-                                <option>-- Pilh untuk mengganti --</option>
-                                <?php include 'config.php';
-                                $sql2  		= "SELECT unit_nama FROM unit";                        
-                                $query2  	= mysqli_query($kon, $sql2);
-                                while ($data2 = mysqli_fetch_array($query2)){
-                                      echo '<option value="'.$data2['unit_nama'].'">'.$data2['unit_nama'].'</option>';
-                                      } 
-                                ?>
+                                <option value="" disabled>-- Pilih status Kerjasama --</option>
+                                <option value="aktif">aktif</option>
+                                <option value="nonaktif">Nonaktif</option>
                               </select>
                             </div>
                           </div>
@@ -105,22 +166,14 @@ if (empty($_SESSION['username'])) {
                           <div class="form-group row">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Deskripsi Kerjasama</label>
                             <div class="col-sm-9">
-                              <input type="text" class="form-control" id="colFormLabel" name="judul_kerjasama"
+                              <input type="text" class="form-control" id="colFormLabel" name="deskripsi_kerjasama"
                                 placeholder="Normal Input" / value="<?= $deskripsi ?>">
                             </div>
                           </div>
 
                           <div class="form-group row">
-                            <label for="colFormLabel" class="col-sm-3 col-form-label">Status</label>
-                            <div class="col-sm-9">
-                              <input type="text" class="form-control" id="colFormLabel" name="judul_kerjasama"
-                                placeholder="Normal Input" / value="<?= $status ?>">
-                            </div>
-                          </div>
-
-                          <div class="form-group row">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Tanggal Awal</label>
-                            <div class="col-sm-9">
+                            <div class="col-sm-5">
                               <div class="input-group ">
                                 <span class="input-group-text"><i data-feather="calendar"></i></span>
                                 <input type="date" id="tawal" class="form-control" value="<?php echo $tanggal_awal;?>"
@@ -128,13 +181,41 @@ if (empty($_SESSION['username'])) {
                               </div>
                             </div>
                           </div>
-                        </div>
 
+                          <div class="form-group row">
+                            <label for="colFormLabel" class="col-sm-3 col-form-label">Tanggal Akhir</label>
+                            <div class="col-sm-5">
+
+                              <div class="input-group ">
+                                <span class="input-group-text"><i data-feather="calendar"></i></span>
+                                <input type="date" id="takhir" class="form-control" value="<?php echo $tanggal_akhir;?>"
+                                  name="takhir" />
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="mb-1">
+                            <P class="form-label" for="file">File Dokumen
+                              Kerjasama</P>
+                            <label for="" style="color:red ;">
+                              * Dokumen Sebelumnya
+                              <b style="color:blue"><?php echo $file;?></b>.
+                            </label>
+                            <div class="input-group ">
+                              <input type="file" name="file" class="form-control-file" id="basicInputFile" />
+                            </div>
+                          </div>
+
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+
+                <div class="col-sm-12 offset-sm-12 modal-footer">
+                  <button type="submit" class="btn btn-info mr-1 btn-sm" name="ubah">Simpan</button>
+                  <button type="reset" class="btn btn-outline-danger btn-sm">Reset</button>
+                </div>
             </form>
           </div>
         </div>
--- admin/kerjasama_proses_edit.php
+++ admin/kerjasama_proses_edit.php
@@ -1,89 +1,80 @@
-<?php
-	session_start();
-	include 'config.php';
-  $id = $_POST['id_kerjasama'];
-  $status = $_POST['status_kerjasama'];
-  $tawal = $_POST['tanggal_awal'];
-  $takhir = $_POST['tanggal_akhir'];
-  $id_dok = $_POST['id_jenis_dok'];
-  $judul = $_POST['judul_kerjasama'];
+<?php 
+session_start();
+if(empty($_SESSION['username'])){
+  echo "<script>alert('Silahkan Login Terlebih Dahulu');window.location='index.php'</script>";
+}
+error_reporting();
+include "config.php";
+
+if (isset($_POST['ubah'])) {
+  $id       = $_POST['id_kerjasama'];
+  $jenis_dok = $_POST['id_jenis_dok'];
+  $fak      = $_POST['id_fak'];
+  $unit     = $_POST['id_unit'];
+  $judul    = $_POST['judul_kerjasama'];
   $deskripsi = $_POST['deskripsi_kerjasama'];
-  $ref = $_POST['no_ref_kerjasama'];
-  $unit = $_POST['id_unit'];
-  $fak = $_POST['id_fak'];
-	$file_suratmasuk			            = $_FILES['file']['name'];
-     date_default_timezone_set('Asia/Jakarta'); 
-		$tanggal_entry  = date("Y-m-d H:i:s");
-        $thnNow = date("Y");
-        $tgl_masuk                  = date('Y-m-d H:i:s', strtotime($tanggalmasuk_suratmasuk));
-        $tgl_surat                  = date('Y-m-d', strtotime($tanggalsurat_suratmasuk));
-        $tgl_disp1                  = date('Y-m-d H:i:s', strtotime($tanggal_disposisi1));
-        $tgl_disp2                  = date('Y-m-d H:i:s', strtotime($tanggal_disposisi2));
-        $tgl_disp3                  = date('Y-m-d H:i:s', strtotime($tanggal_disposisi3));
-	
-	$sql  		= "SELECT * FROM kerjasama where id_kerjasama='".$id."'";                        
-	$query  	= mysqli_query($kon, $sql);
-	$data 		= mysqli_fetch_array($query);
-	
-    //jika gambar tidak ada
-	if ($file_suratmasuk == ''){
-		$ext			= substr($data['file'], strripos($data['file'], '.'));	
-		$nama_b  		= $thnNow.'-'. $ext;
-		rename("aset/".$data['file'], "aset/".$ext);
-		$sql = "UPDATE kerjasama set  id_jenis_dok 			   = '$id_dok',
-                                      id_fak            	 = '$fak',
-                                      id_unit		           = '$unit',
-                                      judul_kerjasama	     = '$judul',
-                                      deskripsi_kerjasama  = '$deskripsi',
-                                      status_kerjasama     = '$status',
-						                          tanggal_awal    	   = '$tawal',
-                                      tanggal_akhir        = '$takhir',
-                                      no_ref_kerjasama     = '$ref',
-                                      date_created         = '$date_created',
-						                          file			= '$ext' 
-				where id_kerjasama = $id";
-				
-		$execute = mysqli_query($kon, $sql);			
-						var_dump($execute);
-            die();
-		echo "<Center><h2><br>Data telah diubah</h2></center>
-		<meta http-equiv='refresh' content='2;url=kerjasama.php'>";
-	}	
-	else{
-        $tipe_file 		= $_FILES['file']['type'];
-        $ukuran_file 	= $_FILES['file']['size'];
-        $tipe_file = array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'rar', 'zip', 'png', 'jpg', 'mp4', 'avi');
-		if (($tipe_file = array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'rar', 'zip', 'png', 'jpg', 'mp4', 'avi')) and ($ukuran_file <= 10340000)){	
-			unlink("aset/".$data['file']);
-			$ext_file		= substr($file_suratmasuk, strripos($file_suratmasuk, '.'));			
-			$tmp_file 		= $_FILES['file']['tmp_name'];
-			
-			$nama_baru = $thnNow.'-'. $ext_file;
-			$path = "aset/".$nama_baru;
-			move_uploaded_file($tmp_file, $path);
-			
-			$sql = "UPDATE kerjasama set  id_jenis_dok 			   = '$id_dok',
-                                      id_fak            	 = '$fak',
-                                      id_unit		           = '$unit',
-                                      judul_kerjasama	     = '$judul',
-                                      deskripsi_kerjasama  = '$deskripsi',
-                                      status_kerjasama     = '$status',
-						                          tanggal_awal    	   = '$tawal',
-                                      tanggal_akhir        = '$takhir',
-                                      no_ref_kerjasama     = '$ref',
-                                      date_created         = '$date_created',
-                                      file			= '$nama_baru' 
-				where id_kerjasama = $id";
-				
-			$execute = mysqli_query($kon, $sql);			
-		
-			echo "<Center><h2><br>Data  telah diubah</h2></center>
-				<meta http-equiv='refresh' content='2;url=kerjasama.php'>";			
-		}
-		else{
-			echo "<Center><h2><br>File yang anda masukkan tidak sesuai ketentuan<br>Silahkan Ulangi</h2></center>
-      <meta http-equiv='refresh' content='2;url=kerjasama.php'>";			
-		}
-	
-	}
-	?>
+  $status    = $_POST['status'];
+  $tanggal_awal   = $_POST['tawal'];
+  $tanggal_akhir  = $_POST['takhir'];
+  $noref          = $_POST['no_ref_kerjasama'];
+  // $file         = $data['file'];
+  
+  $gambar = $_FILES['file']['name'];
+  if ($gambar != "") {
+    $ekstensi_diperbolehkan = array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'rar', 'zip');
+    $x = explode('.', $gambar);
+    $ekstensi = strtolower(end($x));
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $angka_acak = rand(1,999);
+
+    $namabaru = $angka_acak.'-'.$gambar;
+
+    if (in_array($ekstensi, $ekstensi_diperbolehkan)) {
+      move_uploaded_file($file_tmp, '../dok/'. $namabaru);
+
+      $query = "UPDATE kerjasama SET id_jenis_dok='$jenis_dok',
+                                          id_fak='$fak',
+                                          id_unit='$unit',
+                                          judul_kerjasama='$judul',
+                                          deskripsi_kerjasama='$deskripsi',
+                                          status_kerjasama='$status',
+                                          tanggal_awal='$tanggal_awal',
+                                          tanggal_akhir='$tanggal_akhir',
+                                          no_ref_kerjasama='$noref',
+                                          file ='$namabaru' WHERE id_kerjasama='$id'";
+      $result = mysqli_query($kon, $query); 
+      // echo '<pre>';
+      // print_r($result);
+      // die();
+      // echo '</pre>';
+      if (!$result) {
+        die ("Query gagal dijalankan: ".mysqli_errno($kon).
+                             " - ".mysqli_error($kon));
+      }else {
+      //tampil alert dan akan redirect ke halaman index.php
+      //silahkan ganti index.php sesuai halaman yang akan dituju
+      echo "<script>alert('Data berhasil diubah.');window.location='kerjasama.php';</script>";
+      }
+    } else {     
+      //jika file ekstensi tidak jpg dan png maka alert ini yang tampil
+         echo "<script>alert('Ekstensi gambar yang boleh hanya jpg atau png.');window.location='kerjasama_edit.php';</script>";
+     }
+    
+  }else {
+    // jalankan query UPDATE berdasarkan ID yang produknya kita edit
+    $query  = "UPDATE kerjasama SET id_jenis_dok='$jenis_dok', id_fak='$fak', id_unit='$unit', 	judul_kerjasama='$judul', deskripsi_kerjasama='$deskripsi', status_kerjasama='$status', tanggal_awal='$tanggal_awal', tanggal_akhir='$tanggal_akhir', no_ref_kerjasama='$noref' WHERE id_kerjasama='$id'";
+    // $query .= "WHERE id = '$id'";
+    $result = mysqli_query($kon, $query);
+    // periska query apakah ada error
+    if(!$result){
+          die ("Query gagal dijalankan: ".mysqli_errno($kon).
+                           " - ".mysqli_error($kon));
+    } else {
+      //tampil alert dan akan redirect ke halaman index.php
+      //silahkan ganti index.php sesuai halaman yang akan dituju
+        echo "<script>alert('Data berhasil diubah.');window.location='kerjasama.php';</script>";
+    }
+  }
+} 
+
+?>
